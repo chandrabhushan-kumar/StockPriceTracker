b4a3418ffd765d21ce6dde961967a7aeb56b076a
kumar30895@gmail.com <kumar30895@gmail.com>
2023-09-26 22:49:13 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+//import Registration from './Components/Registration/Registration'
+import Stock from './Components/Stock';
+
+function App() {
+  return (
+    <div>
+    {/* <Registration /> */}
+     This is redux
+     {/* <Stock /> */}
+    </div>
+  );
+}
+
+export default App;
